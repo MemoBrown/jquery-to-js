1f02a0a26b25d72bbb33078f7788c4c2fcf10726
Guillermo Moreno <contact@memobrown.com>
2020-07-29 15:09:14 -0500

    Se agregan selectores en home.js para modificar DOM

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,33 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
+(async function load(){
+  // await 
+  // accion
+  // drama
+  // animation
+  async function getData(url){
+    const response = await fetch(url)
+    const data = await response.json()
+    return data;
+  }
+  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
+  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama')
+  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
+  console.log(actionList, dramaList, animationList)
 
-let cambia = "@LeonidasEsteban"
+  const $actionContainer = document.querySelector('#action')
+  const $dramaContainer = document.getElementById('#drama')
+  const $animationContainer = document.getElementById('#animation')
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
-}
+
+  const $featuringContainer = document.getElementById('#featuring')
+  const $form = document.getElementById('#form')
+  const $home = document.getElementById('#home')
+
+
+  const $modal = document.getElementById('modal');
+  const $overlay = document.getElementById('overlay')
+  const $hideModal = document.getElementById('hide-modal')
+
+  const $modalTitle = $modal.querySelector('h1');
+  const $modalImage = $modal.querySelector('img');
+  const $modalDescription = $modal.querySelector('p');
+  })()
