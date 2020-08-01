df2aee7d5a8c69005ca5419310b40a686d627857
Guillermo Moreno <contact@memobrown.com>
2020-07-31 19:51:31 -0500

    Manejo de errores

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,10 @@
   async function getData(url){
     const response = await fetch(url)
     const data = await response.json()
-    return data;
+    if(data.data.movie_count > 0){
+      return data;
+    }
+    throw new Error('No se encontró ningún resultado');
   }
   const $form = document.getElementById('form');
   const $home = document.getElementById('home');
@@ -41,13 +44,23 @@
     $featuringContainer.append($loader)
 
     const data = new FormData($form);
-    const {
-      data: {
-        movies: pelis
-      }
-    } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
-    const HTMLString = featuringTemplate(pelis[0]);
-    $featuringContainer.innerHTML = HTMLString;
+    try {
+      const {
+        data: {
+          movies: pelis
+        }
+      } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+      const HTMLString = featuringTemplate(pelis[0]);
+      $featuringContainer.innerHTML = HTMLString;
+    } catch(error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'No se encontró ningún resultado',
+      });
+      $loader.remove();
+      $home.classList.remove('search-active')
+    }
   })
 
   function videoItemTemplate(movie, category){
